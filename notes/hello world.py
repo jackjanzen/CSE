b872e63b48aeded4a774f86feff90646ddb28add
jackjanzen <42385903+jackjanzen@users.noreply.github.com>
2018-10-24 09:46:02 -0700

    Added first notes

--- notes/hello world.py
+++ notes/hello world.py
@@ -25,3 +25,52 @@ print()
 print(6%2)
 print(5%2)
 print(11%4) # Modulus (Remainder)
+print()
+#variables for text
+name=input("What is your name?")
+print("Hello Daddy %s" % name)
+age=input("How old are you? >_")
+print("%s?!? You belong in a museum." % age)
+print("%s is really old. They are %s years old." % (name,age))
+
+car_name= "free candy and puppies inside"
+car_type= "white van"
+car_cylinders= 16
+car_mpg= 0.01
+
+print("I have a car with %s! It is a %s. Want to ride?" % (car_name, car_type))
+print()
+print()
+print("Hello, I'm Chris Hansen, and welcome to To Catch a Predator Episode 9.")
+#recasting
+real_age=int(input("How old are you again?"))
+hidden_age=real_age+10
+print("This is your real age: %d" % hidden_age)
+print("The camera adds ten years.")
+input("Do you want to know why? Type something, anything to learn.")
+print("I AM ALWAYS WATCHING.")
+
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+print("I AM ALWAYS WATCHING.")
+
+
+
+
