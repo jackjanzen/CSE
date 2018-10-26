b6306adbb58909de1a273e5bd38638b0e8c94539
jackjanzen <42385903+jackjanzen@users.noreply.github.com>
2018-10-26 10:39:29 -0700

    Finished Mad Lib

--- notes/Jack Janzen - Mad Lib.py
+++ notes/Jack Janzen - Mad Lib.py
@@ -1,16 +1,15 @@
-noun1 = input("Noun?")
+phenom1 = input("Phenomenon?")
 verb1 = input("Verb?")
-noun2 = input("Noun?")
+noun1 = input("Noun?")
 country1 = input("Country?")
-noun3 = input("Noun?")
-noun4 = input("Noun?")
-noun5 = input("Noun?")
-noun6 = input("Noun?")
+noun2 = input("Noun?")
+
+
 liquid1 = input("Liquid?")
 place1 = input("Place?")
 city1 = input("City?")
 verb2 = input("Past tense verb?")
-noun7 = input("Noun?")
+noun5 = input("Noun?")
 Lthings = input("Plural living things?")
 
 # same Lthings as line 14
@@ -20,14 +19,28 @@ verb3 = input("Verb?")
 place3 = input("Place?")
 place4 = input("Place?")
 place5 = input("Place?")
-phenom1 = input("Phenomenon?")
 phenom2 = input("Phenomenon?")
+phenom3 = input("Phenomenon?")
 print("A few years ago, claiming that %s was a hoax wasn’t really a big deal, since most people wouldn’t %s you and %s "
-      "wasn’t taking that much of a toll on the world." %s (noun1,verb1,noun2))
-
-
-
-
-
+      "wasn’t taking that much of a toll on the world." % (phenom1, verb1, noun1))
+print("Now, %s is incredibly susceptible to these unbacked and biased claims." % country1)
+print("If we don’t stop the idea that nothing is going wrong with the world, pretty soon "
+      "there won’t be any turning back")
+print("After we have crossed that threshold, it won’t be long until we start running out of %s, more than we already "
+      "are." % noun2)
+print("A huge problem that %s presents is the loss of water." % phenom1)
+print("The hotter it gets, the harder it is for water to condense enough to turn into %s, making water "
+      "supplies in places like %s very, very low." % (liquid1, place1))
+print("In mid-January earlier this year, the iconic city of %s only had 100 days of water left." % place2)
+print("Luckily, the water supply was partially %s by a temporary relief from (the) %s, however that is "
+      "no reason to neglect the problem." % (verb2, noun5))
+print("Such a popular place almost ran out of a substance that %s need to survive." % Lthings)
+print("%s can only last 3 days without water, so everyone in %s would have to have gotten to %s in a matter of "
+      "3 days, otherwise, the inevitable happens a lot sooner than normal." % (Lthings, place1, place2,))
+print("The fact that such a famous place could come that close to having no water in the whole city "
+      "really makes you %s." % verb3)
+print("What place is next? %s? %s? %s?" % (place3, place4, place5))
+print("%s is a dangerously unpredictable issue, and it needs to be addressed before the "
+      "whole world runs out, and a mass death on a %s scale occurs." % (phenom2, phenom3))
 
 
