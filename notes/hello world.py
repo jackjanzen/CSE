701cb12134fd099c00d1a749bce22938887e22dc
jackjanzen <42385903+jackjanzen@users.noreply.github.com>
2018-10-30 09:45:59 -0700

    Added to Notes (Loops)

--- notes/hello world.py
+++ notes/hello world.py
@@ -1,3 +1,4 @@
+'''
 print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 #  This is a comment with no effect on code
 # But I can:
@@ -8,10 +9,10 @@ print("Look at what happens here. Is there any space?")
 print()
 print()
 print("There should be a couple of blank lines here.")
-yoink=2
-yeet=2
+yoink = 2
+yeet = 2
 
-#Math
+# Math
 print(3+5)
 print(5-2)
 print(3*4)
@@ -26,25 +27,32 @@ print(6%2)
 print(5%2)
 print(11%4) # Modulus (Remainder)
 print()
-#variables for text
-name=input("What is your name?")
+
+
+# variables for text
+
+
+name = input("What is your name?")
 print("Hello %s" % name)
-age=input("How old are you? >_")
+age = input("How old are you? >_")
 print("%s?!? You belong in a museum." % age)
 print("%s is really old. They are %s years old." % (name,age))
 
-car_name= "free candy and puppies inside"
-car_type= "white van"
-car_cylinders= 16
-car_mpg= 0.01
+car_name = "free candy and puppies inside"
+car_type = "white van"
+car_cylinders = 16
+car_mpg = 0.01
 
 print("I have a car with %s! It is a %s. Want to ride?" % (car_name, car_type))
 print()
 print()
 print("Hello, I'm Chris Hansen, and welcome to To Catch a Predator Episode 9.")
-#recasting
-real_age=int(input("How old are you again?"))
-hidden_age=real_age+10
+
+# recasting
+
+
+real_age = int(input("How old are you again?"))
+hidden_age = real_age+10
 print("This is your real age: %d" % hidden_age)
 print("The camera adds ten years.")
 input("Do you want to know why? Type something, anything to learn.")
@@ -74,6 +82,63 @@ print("I AM ALWAYS WATCHING.")
 print()
 print()
 print("Now back to your regularly scheduled program.")
+'''
+
+# functions
+
+
+def say_it():
+    print("Hello World!")
+
+
+say_it()
+say_it()
+say_it()
+
+print("Slope-Intercept Formula:")
+
+
+def f(x):
+    print(2 * x + 3)
+
+
+f(1)
+f(5)
+f(5000)
+print("Distance:")
+
+
+def distance(x1, y1, x2, y2):
+    dist = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+    print(dist)
+
+
+distance(0, 0, 3, 4)
+distance(0, 0, 5, 12)
+
+
+
+# functions
+
+
+def say_it():
+    print("That's pretty lit")
+
+
+say_it()
+say_it()
+say_it()
+
+# Loops
+"""
+for i in range(10000):
+    say_it()
+for i in range(1000000):
+    print(i+1)
+"""
+
+
+# While Loops
 
 
 
