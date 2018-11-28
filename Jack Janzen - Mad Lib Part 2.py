ffd888fbb02dc3dc1b8ee2e63f2d4f1bd37e7067
jackjanzen <42385903+jackjanzen@users.noreply.github.com>
2018-11-28 09:47:08 -0800

    yeet

---
+++ Jack Janzen - Mad Lib Part 2.py
@@ -0,0 +1,63 @@
+wordlist = []
+wordlist.append(input("Phenomenon?"))
+wordlist.append(input("Verb?"))
+wordlist.append(input("Noun?"))
+wordlist.append(input("Country?"))
+wordlist.append(input("Noun?"))
+wordlist.append(input("Liquid?"))
+wordlist.append(input("Place?"))
+wordlist.append(input("City?"))
+wordlist.append(input("Past tense verb?"))
+wordlist.append(input("Noun?"))
+wordlist.append(input("Plural living things?"))
+# same Lthings as line 12
+wordlist.append(input("Place?"))
+wordlist.append(input("Verb?"))
+wordlist.append(input("Place?"))
+wordlist.append(input("Place?"))
+wordlist.append(input("Place?"))
+wordlist.append(input("Phenomenon?"))
+wordlist.append(input("Phenomenon?"))
+
+"""
+phenom1 = input("Phenomenon?")
+verb1 = input("Verb?")
+noun1 = input("Noun?")
+country1 = input("Country?")
+noun2 = input("Noun?")
+liquid1 = input("Liquid?")
+place1 = input("Place?")
+city1 = input("City?")
+verb2 = input("Past tense verb?")
+noun5 = input("Noun?")
+Lthings = input("Plural living things?")
+# same Lthings as line 32
+place2 = input("Place?")
+verb3 = input("Verb?")
+place3 = input("Place?")
+place4 = input("Place?")
+place5 = input("Place?")
+phenom2 = input("Phenomenon?")
+phenom3 = input("Phenomenon?")
+"""
+print("A few years ago, claiming that %s was a hoax wasn’t really a big deal, since most people wouldn’t %s you and %s "
+      "wasn’t taking that much of a toll on the world." % (wordlist[0], wordlist[1], wordlist[2]))
+print("Now, %s is incredibly susceptible to these unbacked and biased claims." % wordlist[3])
+print("If we don’t stop the idea that nothing is going wrong with the world, pretty soon "
+      "there won’t be any turning back")
+print("After we have crossed that threshold, it won’t be long until we start running out of %s, more than we already "
+      "are." % wordlist[4])
+print("A huge problem that %s presents is the loss of water." % wordlist[5])
+print("The hotter it gets, the harder it is for water to condense enough to turn into %s, making water "
+      "supplies in places like %s very, very low." % (wordlist[6], wordlist[7]))
+print("In mid-January earlier this year, the iconic city of %s only had 100 days of water left." % wordlist[8])
+print("Luckily, the water supply was partially %s by a temporary relief from (the) %s, however that is "
+      "no reason to neglect the problem." % (wordlist[9], wordlist[10]))
+print("Such a popular place almost ran out of a substance that %s need to survive." % wordlist[11])
+print("%s can only last 3 days without water, so everyone in %s would have to have gotten to %s in a matter of "
+      "3 days, otherwise, the inevitable happens a lot sooner than normal." % (wordlist[11], wordlist[12], wordlist[13]))
+print("The fact that such a famous place could come that close to having no water in the whole city "
+      "really makes you %s." % wordlist[14])
+print("What place is next? %s? %s? %s?" % (wordlist[15], wordlist[16], wordlist[17]))
+print("%s is a dangerously unpredictable issue, and it needs to be addressed before the "
+      "whole world runs out, and a mass death on a Black Death scale occurs." % (wordlist[0]))
