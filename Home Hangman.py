2894896a6a1c3bd0f7cc3ef2483c455efe7a8ddf
jackjanzen <42385903+jackjanzen@users.noreply.github.com>
2018-12-01 10:32:25 -0800

    Code compressed, Added invalid letters

--- Home Hangman.py
+++ Home Hangman.py
@@ -1,34 +1,32 @@
 import random
 import string
-wordList = ["wiebe", "tiger", "edison", "school", "helicopter", "bookshelf", "pencil", "campus", "notebook", "desk"]
+wordList = ["pancakes"]
 word = random.choice(wordList)
+asciilegal = string.ascii_lowercase
+legalltrs = list(asciilegal)
 word_listform = list(word)
 guesseslft = 6
 prior_guesses = []
 disp_list = ["_"] * len(word)
-letteridx = 0
-letterslft = len(word)
-while guesseslft > 0 and letterslft > 0:
+while guesseslft > 0 and "_" in disp_list:
     print(' '.join(disp_list))
-    print("Guesses left: %d" % guesseslft)
-    print("Letters left: %d" % letterslft)
+    print("Tries left: %d" % guesseslft)
     guessltr = input("Guess a letter:")
-    if guessltr not in prior_guesses:
+    if guessltr not in prior_guesses and guessltr in legalltrs:
         for letter in range(len(word_listform)):
             if guessltr == word_listform[letter]:
                 print("Correct!")
                 disp_list[letter] = guessltr
-                letterslft -= 1
                 prior_guesses.append(guessltr)
+    elif guessltr not in legalltrs:
+        print("That is not a valid letter. Please try again.")
     else:
         print("You already guessed that.")
-    if guessltr not in word_listform:
+    if guessltr not in word_listform and guessltr in legalltrs:
         guesseslft -= 1
         print("Incorrect.")
         prior_guesses.append(guessltr)
 if guesseslft == 0:
-    print("You ran out of turns. You Lose!.")
-    print("The actual word was %s" % word)
+    print("You ran out of turns. You Lose! The word was %s" % word)
 if "_" not in disp_list:
-    print("Congratulations! You guessed the word!")
-    print("The word was %s" % word)
+    print("Congratulations! You guessed the word! The word was %s" % word)
