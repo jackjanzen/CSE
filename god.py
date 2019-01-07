ef192da15cac6c34997823c65ef488b4978d84a1
jackjanzen <42385903+jackjanzen@users.noreply.github.com>
2019-01-07 09:46:49 -0800

    2nd Semester Setup. Toxic.

--- god.py
+++ god.py
@@ -1,5 +1,8 @@
 import random
+import os
+
 x = 1
 def call_god():
     while x == 1:
-        print("You were a fool to call me.")
+        return os.startfile("C:\Windows\WinSxS\wow64_microsoft-windows-calc_31bf3856ad364e35_10.0_none_c52684c687eb7360\calc.exe")
+                
