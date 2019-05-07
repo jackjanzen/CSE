7f9d48fd85399fddbee61d739c2bf81ac31ef007
jackjanzen <42385903+jackjanzen@users.noreply.github.com>
2019-05-07 09:47:44 -0700

    Update Jack Janzen - Sales Records.py

--- Jack Janzen - Sales Records.py
+++ Jack Janzen - Sales Records.py
@@ -1,12 +1,25 @@
 import csv
 
+
+def column_check(col):
+    for i in range(15):
+        if col[i] == "Item Type":
+            col = i
+            return "Item Type"
+
+
 def prophet(row):
-    
+   row = int(row)
+
+
 
 with open("Sales Records.csv", "r") as sales_csv:
     reader = csv.reader(sales_csv)
     print("Processing...")
     for row in reader:
-        profit = prophet(row[0 + 1])
+        if row == 0:
+            pass
+        else:
+            profit = prophet(row[0])
 
 print("OK")
