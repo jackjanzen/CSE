e86b77280efe1b7caa0af1db5ff88d41ec7c4376
jackjanzen <42385903+jackjanzen@users.noreply.github.com>
2019-05-08 09:47:02 -0700

    Updated Salsa Records

--- Jack Janzen - Sales Records.py
+++ Jack Janzen - Sales Records.py
@@ -1,25 +1,45 @@
 import csv
 
 
-def column_check(col):
-    for i in range(15):
+def prof_col_check(col):
+    num = -1
+    for i in range(len(col)):
+        if col[i] == "Total Profit":
+            num = i
+    return num
+
+
+def type_col_check(col):
+    name = -1
+    for i in range(len(col)):
         if col[i] == "Item Type":
-            col = i
-            return "Item Type"
+            name = i
+    return name
 
 
-def prophet(row):
-   row = int(row)
+def prophet(thing):
+    thing = float(thing)
+    return thing
 
 
+# def nombre(itemtype):
+#     pass
+#     itemtype
 
 with open("Sales Records.csv", "r") as sales_csv:
     reader = csv.reader(sales_csv)
     print("Processing...")
+    totalprofit = 0
+    itemtype = []
     for row in reader:
-        if row == 0:
+        if row[0] == "Region":
             pass
         else:
-            profit = prophet(row[0])
-
+            itemtype.append(type_col_check(row))
+            profit = prophet(row[prof_col_check(row)])
+            totalprofit += profit
+print("Total Profit:")
+print("${:,}".format(totalprofit))
+print(itemtype[0])
 print("OK")
+
