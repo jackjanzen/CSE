1f76e2fea5f2e2341bf0095ebb46ac085455a705
jackjanzen <42385903+jackjanzen@users.noreply.github.com>
2019-05-09 20:53:54 -0700

    Sales Records Complete

--- Jack Janzen - Sales Records.py
+++ Jack Janzen - Sales Records.py
@@ -1,143 +1,27 @@
 import csv
 
+items = {}
+recorder = 0
+high_type = ""
 
-def prof_col_check(col):
-    num = -1
-    for i in range(len(col)):
-        if col[i] == "Total Profit":
-            num = i
-    return num
-
-
-# def type_col_check(col):
-#     name = -1
-#     for i in range(len(col)):
-#         if col[i] == "Item Type":
-#             name = i
-#     return name
-#
-# def typewriter(thing):
-#     pass
-
-
-def prophet(thing):
-    thing = float(thing)
-    return thing
-
-
-# def nombre(itemtype):
-#     pass
-#     itemtype
-
-with open("Sales Records.csv", "r") as sales_csv:
-    reader = csv.reader(sales_csv)
-    print("Processing...")
-    totalprofit = 0
-    itemtype = []
-    fruits = 0
-    clothes = 0
-    meat = 0
-    beverages = 0
-    office = 0
-    cosmetics = 0
-    snacks = 0
-    personal = 0
-    household = 0
-    vegetables = 0
-    baby = 0
-    cereal = 0
+with open("Sales Records.csv", "r") as old_csv:
+    print("Processing Data...")
+    reader = csv.reader(old_csv)
     for row in reader:
-        if row[0] == "Region":
-            pass
-        else:
-            a = False
-            b = False
-            c = False
-            d = False
-            e = False
-            f = False
-            g = False
-            h = False
-            i = False
-            j = False
-            k = False
-            ll = False
-            if row[2] in itemtype:
-                pass
-            elif row[2] == 'Fruits' and fruits not in itemtype:
-                itemtype.append(fruits)
-                a = True
-            elif row[2] == 'Clothes' and clothes not in itemtype:
-                itemtype.append(clothes)
-                b = True
-            elif row[2] == 'Meat' and meat not in itemtype:
-                itemtype.append(meat)
-                c = True
-            elif row[2] == 'Beverages' and beverages not in itemtype:
-                itemtype.append(beverages)
-                d = True
-            elif row[2] == 'Office Supplies' and office not in itemtype:
-                e = True
-                itemtype.append(office)
-            elif row[2] == 'Cosmetics' and cosmetics not in itemtype:
-                f = True
-                itemtype.append(cosmetics)
-            elif row[2] == 'Snacks' and snacks not in itemtype:
-                g = True
-                itemtype.append(snacks)
-            elif row[2] == 'Personal Care' and personal not in itemtype:
-                h = True
-                itemtype.append(personal)
-            elif row[2] == 'Household' and household not in itemtype:
-                i = True
-                itemtype.append(household)
-            elif row[2] == 'Vegetables' and vegetables not in itemtype:
-                j = True
-                itemtype.append(vegetables)
-            elif row[2] == 'Baby Food' and baby not in itemtype:
-                k = True
-                itemtype.append(baby)
-            elif row[2] == 'Cereal' and cereal not in itemtype:
-                last = True
-                itemtype.append(cereal)
-            if a is True:
-                profit = prophet(row[prof_col_check(row)])
-                fruits += profit
-            elif b is True:
-                profit = prophet(row[prof_col_check(row)])
-                clothes += profit
-            elif c is True:
-                profit = prophet(row[prof_col_check(row)])
-                meat += profit
-            elif d is True:
-                profit = prophet(row[prof_col_check(row)])
-                beverages += profit
-            elif e is True:
-                profit = prophet(row[prof_col_check(row)])
-                office += profit
-            elif f is True:
-                profit = prophet(row[prof_col_check(row)])
-                cosmetics += profit
-            elif g is True:
-                profit = prophet(row[prof_col_check(row)])
-                snacks += profit
-            elif h is True:
-                profit = prophet(row[prof_col_check(row)])
-                personal += profit
-            elif i is True:
-                profit = prophet(row[prof_col_check(row)])
-                household += profit
-            elif j is True:
-                profit = prophet(row[prof_col_check(row)])
-                vegetables += profit
-            elif k is True:
-                profit = prophet(row[prof_col_check(row)])
-                baby += profit
-            elif ll is True:
-                profit = prophet(row[prof_col_check(row)])
-                cereal += profit
-print("Total Profit:")
-print("${:,}".format(totalprofit))
-print(itemtype)
-print("OK")
+        if row[0] == 'Region':
+            continue
+        item_type = row[2]
+        profit = float(row[13])
+        try:
+            items[item_type] += profit
+        except KeyError:
+            items[item_type] = profit
 
+for key, item in items.items():
+    print(key + ": ")
+    print("${:,}".format(round(item, 2)))
+    if item > recorder:
+        recorder = item
+        high_type = key
+
+print("Most Profitable Item: " + high_type + " with a profit of " + "${:,}".format(round(recorder, 2)))
